c1ad5a3ec5ecf0b16ba6bec50a765d63125c45d2
david <dscarso@advantive.com.ar>
2020-12-18 14:49:48 -0300

    ma cosas

---
+++ strings.py
@@ -0,0 +1,38 @@
+myStr = "Hello world"
+
+#Veremos todoslo metodos y propiedades del string
+# print(dir(myStr))
+
+
+# print(myStr.lower())
+# print(myStr.upper())
+# print(myStr.capitalize())
+# print(myStr.title())
+# print(myStr.swapcase())
+
+
+# print(myStr.replace('hello', 'bye'))
+# print(myStr.replace('Hello', 'bye'))
+
+# print(myStr.count('l'))
+
+# print(myStr.startswith('hello'))
+# print(myStr.startswith('Hello'))
+
+# print(myStr.split())
+# print(myStr.split('o'))
+
+# print(myStr.find('o')) #posición (Indice) de la letra
+# print(len(myStr))
+# print(myStr.index('e'))
+# print(myStr.isnumeric())
+# print(myStr.isalpha())
+
+print(myStr[4])
+
+
+
+
+
+
+
