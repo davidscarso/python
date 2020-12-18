bb2e1a1d34c6c422a20854e4f112f82055936aa9
david <dscarso@advantive.com.ar>
2020-12-18 14:31:35 -0300

    More examples

--- datatype.py
+++ datatype.py
@@ -1,4 +1,29 @@
-print("Hello world")
-print('Hello world')
-print('''Hello world''')
-print("""Hello world""")
+
+#String str
+print(type("Hello world"))
+
+#Integet int
+print(type(1))
+
+#Float float
+print(type(1.2))
+
+#Boolean bool
+print(type(True))
+print(type(False))
+
+#List list
+print(type([12,20,30]))
+
+#Tuples (Es como enumerado)
+print(type((12,20,30)))
+
+#Dictionaries dict
+print(type({
+    "name ":"Fulanito",
+    "lastname":"Cosmes",
+    "nickname":"Pepo"
+}))
+
+#NoneType
+print(type(None))
---
+++ variables.py
@@ -0,0 +1,24 @@
+x = 10
+X = 10
+_2x = 10
+
+name = "Fazt"
+print(name)
+
+# I can define everything together
+
+a, libro = 10, 'super Libro'
+
+print(a, libro)
+print(a)
+print(libro)
+
+
+#Conventions
+book_name="Snake Case"
+bookName="camel Case"
+BookName=" Pascal Case"
+
+#constan (todo en mayusculas)
+PI = 3.1416
+MY_NAME="Pepe"
